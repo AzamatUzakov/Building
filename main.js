6872302267ca652741ee31d1dd87150670e01aeb
AzamatUzakov <azamatakmal053@gmail.com>
2024-01-05 00:02:46 +0500

    начал делать анимацию второго блока

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 
-  AOS.init();
+AOS.init();
+
+
+let one_arch_img = document.querySelector('.one_arch_img')
+
+
+/* 
+window.onscroll = () => {
+    let y = window.scrollY
+    stars.style.margin = (y * 0.45) + "px"
+    moon.style.marginTop = (y * 1.30) + "px"
+    ges.style.marginTop = (y * 0.55) + "px"
+    text.style.marginRight = (y * 0.70) + "px"
+    btn.style.marginTop = (y * 1) + "px"
+} */
+
+window.onscroll = () => {
+    let y = window.scrollY
+
+    one_arch_img.style.borderRadius = (y * 0.50) + "%"
+
+}
