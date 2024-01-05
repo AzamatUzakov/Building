5af67dd1cf33121dfc9edd57f96d0fe9f1e36014
AzamatUzakov <azamatakmal053@gmail.com>
2024-01-05 16:05:54 +0500

    проверочная анимация второго блока

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 
-AOS.init();
+AOS.init(
+    //duration: 400
+);
 
 
-let one_arch_img = document.querySelector('.one_arch_img')
+let one_arch_img = document.querySelector('.one_arch_img_')
+let arch_titile = document.querySelector('.arch_titile')
 
 
+/* 
+
+setTimeout(() => {
+    arch_titile.style.display = "block"
+    arch_titile.style.scale = 1
+    arch_titile.style. translateY =(0 + "px")
+
+}, 1000);
+ */
 /* 
 window.onscroll = () => {
     let y = window.scrollY
@@ -14,10 +26,10 @@ window.onscroll = () => {
     text.style.marginRight = (y * 0.70) + "px"
     btn.style.marginTop = (y * 1) + "px"
 } */
-
+/* 
 window.onscroll = () => {
     let y = window.scrollY
 
-    one_arch_img.style.borderRadius = (y * 0.50) + "%"
+    one_arch_img.style.borderRadius = (y * 10) + "%"
 
-}
+} */
